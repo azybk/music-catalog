2d54fbcf2b468649d69b9d08b071adf66b727d2e
azy bk <kumbaraazy@gmail.com>
2025-05-10 13:13:09 +0700

    initialize config

---
+++ pkg/internalsql/sql.go
@@ -0,0 +1,17 @@
+package internalsql
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect(dataSourceName string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dataSourceName))
+	if err != nil {
+		log.Fatalf("error connect to database: %+v\n", db)
+		return nil, err
+	}
+	return db, nil
+}
